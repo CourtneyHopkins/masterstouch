1b98a13c6ab1554bdad4dff5774f5829519d7e48
Courtneyjones <me@courtneyejones.com>
2013-12-09 01:47:25 -0500

    Initial commit with additional page templates and minor changes

---
+++ footer.php
@@ -0,0 +1,12 @@
+			<footer class="footer" role="contentinfo">
+				<div id="inner-footer" class="wrap clearfix">
+					<nav role="navigation">
+						<?php bones_footer_links(); ?>
+					</nav>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+				</div>
+			</footer>
+		</div>
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title><?php wp_title(''); ?></title>
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<div id="container">
+			<header class="header" role="banner">
+				<div id="inner-header" class="wrap clearfix">
+					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<nav role="navigation">
+						<?php bones_main_nav(); ?>
+					</nav>
+				</div>
+			</header>
